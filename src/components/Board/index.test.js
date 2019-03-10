95f6dfbc8458d767449c1af2871ebf1f686c5df8
Carmen <ameliacarmenholmes@gmail.com>
2019-03-10 11:50:05 -0400

    Add Function To Determine Winner
    Add tests for function
    Add missing test for square click

--- src/components/Board/index.test.js
+++ src/components/Board/index.test.js
@@ -13,4 +13,23 @@ describe('Board', () => {
     const wrapper = shallow(<Board />);
     expect(wrapper.find('.game-square').children(Square).length).toEqual(9);
   });
+
+  it('handles square click', () => {
+    const wrapperInstance = shallow(<Board />).instance();
+    wrapperInstance.handleClick(0);
+    expect(wrapperInstance.state.squares[0]).toBe('X');
+  });
 });
+
+describe('calculateWinner', () => {
+  it('should return null if there is no winner', () => {
+    const noWinner =  ["X", "X", "O", "O", "O", "X", "X", "O", "X"];
+    const wrapperInstance = shallow(<Board />).instance();
+    expect(wrapperInstance.calculateWinner(noWinner)).toBe(null);
+  })
+  it('should return winner if there is a winner', () => {
+    const winner = ["X", "X", "O", "X", "O", "X", "O", "O", "X"]
+    const wrapperInstance = shallow(<Board />).instance();
+    expect(wrapperInstance.calculateWinner(winner)).toEqual('O')
+  })
+})
